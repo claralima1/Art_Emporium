71d564feee91bc9dba1c76c80a24cd1114271caa
Clara Lima <claralima601@gmail.com>
2022-07-14 10:32:56 -0300

    Att do projeto prof Ewerton

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
 from django.urls import path
+from .views import HomeView
 
 urlpatterns = [
-    path('home/', home.site.urls), path('contato/', contato.site.urls),path('sobre/', sobre.site.urls),path('teste/', teste.site.urls),
+    path('', HomeView.as_view()), 
 ]
